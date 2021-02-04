22b566a30a767b67303c30e094a9670d2b9683e3
Mike Harris <MikeMKH@users.noreply.github.com>
2021-02-04 06:26:40 -0600

    add ch 5.2

--- ch05/Examples/Examples.cs
+++ ch05/Examples/Examples.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net; // WebClient
 using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Examples
@@ -11,7 +12,7 @@ namespace Examples
         public async void HttpClientExampleAsync()
         {
             var client = new HttpClient();
-            var site = await client.GetStringAsync("https://rancidrancid.com/");
+            var site = await client.GetStringAsync("https://www.instagram.com/mikemkh8/?hl=en");
             Assert.True(site.Length > 0);
         }
         
@@ -20,8 +21,23 @@ namespace Examples
         {
             var client = new WebClient();
             // not async
-            var site = client.DownloadString("https://rancidrancid.com/");
+            var site = client.DownloadString("https://racket-lang.org/");
             Assert.True(site.Length > 0);
+        }
+        
+        [Fact]
+        public async void AsynchronousBoundariesExample()
+        {
+            var client = new HttpClient();
+            
+            Task<int> length = GetPageLengthAsync("https://www.haskell.org/");
+            Assert.True(length.Result > 0);
+            
+            async Task<int> GetPageLengthAsync(string url)
+            {
+                Task<string> result = client.GetStringAsync(url);
+                return (await result).Length;
+            }
         }
     }
 }

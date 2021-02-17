2f63690fb910170a80f69577dbc80be6f951888e
Mike Harris <MikeMKH@users.noreply.github.com>
2021-02-17 06:31:24 -0600

    add ch 5.9

---
+++ ch05/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"processing...{string.Join(",", args)}");
+            await Task.Delay(1000);
+            Console.WriteLine($"Thank you for playing Wing Commander!");
+        }
+        /*
+        ⋊> ~/K/c/c/app on main ⨯ dotnet run                                                                                      06:28:12
+        processing...
+        Thank you for playing Wing Commander!
+        ⋊> ~/K/c/c/app on main ⨯ dotnet run hello                                                                                06:28:26
+        processing...hello
+        Thank you for playing Wing Commander!
+        ⋊> ~/K/c/c/app on main ⨯ dotnet run hello how are you                                                                    06:28:43
+        processing...hello,how,are,you
+        Thank you for playing Wing Commander!
+        */
+    }
+}

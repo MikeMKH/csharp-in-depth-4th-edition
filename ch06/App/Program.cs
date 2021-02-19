e40979175bc34dd3ddf09aa1b5830fa837299501
Mike Harris <MikeMKH@users.noreply.github.com>
2021-02-19 06:38:24 -0600

    add ch 6.1

---
+++ ch06/App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAndWait(TimeSpan.FromMilliseconds(100));
+        }
+        
+        static async Task PrintAndWait(TimeSpan delay)
+        {
+            Console.WriteLine("before delays");
+            await Task.Delay(delay);
+            Console.WriteLine("between delays");
+            await Task.Delay(delay);
+            Console.WriteLine("after delays");
+        }
+    }
+}

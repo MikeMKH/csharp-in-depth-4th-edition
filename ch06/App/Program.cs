c681749da43f92e6d1abe4185d798b3428ee79e8
Mike Harris <MikeMKH@users.noreply.github.com>
2021-02-22 06:24:00 -0600

    add ch 6.1.3 - 6.1.4

--- ch06/App/Program.cs
+++ ch06/App/Program.cs
@@ -5,11 +5,29 @@ namespace App
 {
     class Program
     {
+        /*
+        private static void _003CMain_003E (string[] args)
+        {
+        	Main (args).GetAwaiter ().GetResult ();
+        }
+        */
         static async Task Main(string[] args)
         {
             await PrintAndWait(TimeSpan.FromMilliseconds(100));
         }
         
+        /*
+        [AsyncStateMachine (typeof(_003CPrintAndWait_003Ed__1))]
+        private static Task PrintAndWait (TimeSpan delay)
+        {
+        	_003CPrintAndWait_003Ed__1 stateMachine = default(_003CPrintAndWait_003Ed__1);
+        	stateMachine._003C_003Et__builder = AsyncTaskMethodBuilder.Create ();
+        	stateMachine.delay = delay;
+        	stateMachine._003C_003E1__state = -1;
+        	stateMachine._003C_003Et__builder.Start (ref stateMachine);
+        	return stateMachine._003C_003Et__builder.Task;
+        }
+        */
         static async Task PrintAndWait(TimeSpan delay)
         {
             Console.WriteLine("before delays");
@@ -19,4 +37,84 @@ namespace App
             Console.WriteLine("after delays");
         }
     }
+    
+    /*
+    [StructLayout (LayoutKind.Auto)]
+    [CompilerGenerated]
+    private struct _003CPrintAndWait_003Ed__1 : IAsyncStateMachine
+    {
+    	public int _003C_003E1__state;
+    
+    	public AsyncTaskMethodBuilder _003C_003Et__builder;
+    
+    	public TimeSpan delay;
+    
+    	private TaskAwaiter _003C_003Eu__1;
+    
+    	private void MoveNext ()
+    	{
+    		int num = _003C_003E1__state;
+    		try {
+    			TaskAwaiter awaiter;
+    			if (num != 0) {
+    				if (num == 1) {
+    					awaiter = _003C_003Eu__1;
+    					_003C_003Eu__1 = default(TaskAwaiter);
+    					num = (_003C_003E1__state = -1);
+    					goto IL_00db;
+    				}
+    				Console.WriteLine ("before delays");
+    				awaiter = Task.Delay (delay).GetAwaiter ();
+    				if (!awaiter.IsCompleted) {
+    					num = (_003C_003E1__state = 0);
+    					_003C_003Eu__1 = awaiter;
+    					_003C_003Et__builder.AwaitUnsafeOnCompleted (ref awaiter, ref this);
+    					return;
+    				}
+    			} else {
+    				awaiter = _003C_003Eu__1;
+    				_003C_003Eu__1 = default(TaskAwaiter);
+    				num = (_003C_003E1__state = -1);
+    			}
+    			awaiter.GetResult ();
+    			Console.WriteLine ("between delays");
+    			awaiter = Task.Delay (delay).GetAwaiter ();
+    			if (!awaiter.IsCompleted) {
+    				num = (_003C_003E1__state = 1);
+    				_003C_003Eu__1 = awaiter;
+    				_003C_003Et__builder.AwaitUnsafeOnCompleted (ref awaiter, ref this);
+    				return;
+    			}
+    			goto IL_00db;
+    			IL_00db:
+    			awaiter.GetResult ();
+    			Console.WriteLine ("after delays");
+    		} catch (Exception exception) {
+    			_003C_003E1__state = -2;
+    			_003C_003Et__builder.SetException (exception);
+    			return;
+    		}
+    		_003C_003E1__state = -2;
+    		_003C_003Et__builder.SetResult ();
+    	}
+    
+    	void IAsyncStateMachine.MoveNext ()
+    	{
+    		//ILSpy generated this explicit interface implementation from .override directive in MoveNext
+    		this.MoveNext ();
+    	}
+    
+    	[DebuggerHidden]
+    	private void SetStateMachine (IAsyncStateMachine stateMachine)
+    	{
+    		_003C_003Et__builder.SetStateMachine (stateMachine);
+    	}
+    
+    	void IAsyncStateMachine.SetStateMachine (IAsyncStateMachine stateMachine)
+    	{
+    		//ILSpy generated this explicit interface implementation from .override directive in SetStateMachine
+    		this.SetStateMachine (stateMachine);
+    	}
+    }
+    */
 }

0798bd61b293151f47a03e9120b59be6fd7eb07f
Mike Harris <MikeMKH@users.noreply.github.com>
2021-03-04 06:19:50 -0600

    add ch 8.3

--- ch08/Examples/Examples.cs
+++ ch08/Examples/Examples.cs
@@ -2,19 +2,20 @@ using System;
 using Xunit;
 
 namespace Examples
-{
+{       
+        
     public class Examples
     {
-        struct Point
+        public struct Point
         {
             public double X { get; }
             public double Y { get; }
             
-            public Point(double x, double y)
-            {
-                X = x;
-                Y = y;
-            }
+            public Point(double x, double y) => (X, Y) = (x, y);
+            
+            public static Point Add(Point left, Point right) => left + right;
+            public static Point operator+(Point left, Point right)
+              => new Point(left.X + right.X, left.Y + right.Y);
         }
         
         [Fact]
@@ -31,6 +32,15 @@ namespace Examples
             Assert.Equal(p3, p3);
             Assert.NotEqual(p1, p3);
             Assert.NotEqual(p2, p3);
+        }
+        
+        [Fact]
+        public void OperationUsingExpressionBodyMethodExample()
+        {
+            var p1 = new Point(1, 2);
+            var p2 = new Point(3, 4);
+            Assert.Equal(new Point(4, 6), p1 + p2);
+            Assert.Equal(Point.Add(p1, p2), p1 + p2);
         }
     }
 }

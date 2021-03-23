b3ec80025ed3000ec6276cf62d9a703f294c5c44
Mike Harris <MikeMKH@users.noreply.github.com>
2021-03-23 06:27:50 -0500

    add ch 11.3.1 - 11.3.3

--- ch11/Examples/Examples.cs
+++ ch11/Examples/Examples.cs
@@ -58,5 +58,44 @@ namespace Examples
                 }
             }
         }
+        
+        [Fact]
+        public void TypesOfTupleLiteralsExamples()
+        {
+            var tuple1 = (x: 10, 20);
+            (int x, int) tuple2 = (x: 10, 20);
+            Assert.IsType<(int x, int)>(tuple1);
+            Assert.Equal(tuple1, tuple2);
+            
+            var array1 = new [] {("a", 10)};
+            (string, int) [] array2 = new [] {("a", 10)};
+            Assert.IsType<(string, int)[]>(array1);
+            Assert.Equal(array1, array2);
+        }
+        
+        [Fact]
+        public void ConversionsFromTupleLiteralsExamples()
+        {
+            (byte, object) t1 = (8, "tuple");
+            
+            Assert.Equal(8, t1.Item1);
+            
+            int x = Byte.MaxValue + 1;
+            var t2 = ((byte, string)) (x, "tuple");
+            var t3 = ((byte) x, "tuple");
+            
+            Assert.NotEqual(Byte.MaxValue + 1, t2.Item1);
+            Assert.Equal(0, t2.Item1);
+            
+            Assert.NotEqual(Byte.MaxValue + 1, t3.Item1);
+            Assert.Equal(0, t3.Item1);
+            
+            var z = (x, "hello");
+            (long, string) z1 = z;
+            // (byte, string) z2 = z; // error CS0266: Cannot implicitly convert type '(int x, string)' to '(byte, string)'
+            (byte, string) z3 = ((byte, string)) z;
+            (object, object) z4 = z;
+            // (string, string) z5 = ((string, string)) z; // error CS0030: Cannot convert type '(int x, string)' to '(string, string)'
+        }
     }
 }

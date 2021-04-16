b04cf87427ceef1018e3f5e5836e9441e77f24bf
Mike Harris <MikeMKH@users.noreply.github.com>
2021-04-16 06:36:30 -0500

    add ch 13.3

--- ch13/Examples/Examples.cs
+++ ch13/Examples/Examples.cs
@@ -125,5 +125,57 @@ namespace Examples
             Assert.Equal(42, x);
             // (GetFieldAlias())++; // error CS8331: Cannot assign to method 'Examples.GetFieldAlias()' because it is a readonly variable
         }
+        
+        [Fact]
+        public void InParameterExample()
+        {
+            DateTime date = DateTime.Parse("2020-04-28");
+            var s1 = Format(date);
+            var s2 = Format(in date);
+            var s3 = Format(date.AddMinutes(1));
+            // var s4 = Format(in date.AddMinutes(1)); // error CS8156: An expression cannot be used in this context because it may not be passed or returned by reference
+            
+            Assert.Equal("28/04/2020", s1);
+            Assert.Equal("28/04/2020", s2);
+            Assert.Equal("28/04/2020", s3);
+            
+            string Format(in DateTime value)
+            {
+                var text = value.ToString(
+                    "dd/MM/yyyy",
+                    System.Globalization.CultureInfo.InvariantCulture);
+                return text;
+            }
+        }
+        
+        [Fact]
+        public void InParameterStrangeExample()
+        {
+            const int VALUE = 8;
+            int x = VALUE, y = x;
+            
+            Assert.Equal(VALUE, x);
+            Assert.Equal(VALUE, y);
+            
+            InParameter(x, () => x++);
+            ValueParameter(y, () => y++);
+            
+            Assert.Equal(VALUE + 1, x);
+            Assert.Equal(VALUE + 1, y);
+            
+            void InParameter(in int p, Action action)
+            {
+                Assert.Equal(VALUE, p);
+                action();
+                Assert.Equal(VALUE + 1, p);
+            }
+            
+            void ValueParameter(int p, Action action)
+            {
+                Assert.Equal(VALUE, p);
+                action();
+                Assert.Equal(VALUE, p); // strange
+            }
+        }
     }
 }

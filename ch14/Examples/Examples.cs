0326dc480bec6ffcd76c0887a8ab7da0fae06bfd
Mike Harris <MikeMKH@users.noreply.github.com>
2021-04-23 06:23:53 -0500

    add ch 14.2 - 14.3

--- ch14/Examples/Examples.cs
+++ ch14/Examples/Examples.cs
@@ -19,7 +19,7 @@ namespace Examples
         }
         
         [Fact]
-        public void LocalMethodVaribleMustBeDeclaredBeBeingUsedExample()
+        public void LocalMethodVariableMustBeDeclaredBeBeingUsedExample()
         {
             // Process(ref p); // error CS0841: Cannot use local variable 'p' before it is declared
             int p = 0;
@@ -27,6 +27,34 @@ namespace Examples
             Assert.Equal(1, p);
             
             void Process(ref int x) => x++;
+        }
+        
+        [Fact]
+        public void OutVariableExample()
+        {
+            Assert.Equal(123, ParseInt32("123"));
+            Assert.Equal(-456, ParseInt32("-456"));
+            Assert.Null(ParseInt32("ok"));
+            Assert.Null(ParseInt32("2021-04-22"));
+            Assert.Null(ParseInt32("1.23456789"));
+            
+            Nullable<int> ParseInt32(string str) => Int32.TryParse(str, out int value) ? value : (int?) null;
+        }
+        
+        [Fact]
+        public void IntegerLiteralsExamples()
+        {
+            byte b1 = 0b1000_0001;
+            byte b2 = 0x81;
+            byte b3 = 129;
+            Assert.Equal(b1, b2);
+            Assert.Equal(b2, b3);
+            
+            int maxInt32 = 2_147_483_647;
+            Assert.Equal(Int32.MaxValue, maxInt32);
+            
+            decimal oneMillionDollars = 1_000_000.00m;
+            Assert.Equal(1_000.00m * 1_000.00m, oneMillionDollars);
         }
     }
 }

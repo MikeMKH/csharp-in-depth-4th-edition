41b55999dfb5ca3296a5b4cb6e595c5cf07e4e88
Mike Harris <MikeMKH@users.noreply.github.com>
2021-04-28 06:40:02 -0500

    add ch 15.1

--- ch15/Examples/Examples.cs
+++ ch15/Examples/Examples.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
 
 namespace Examples
@@ -6,9 +7,51 @@ namespace Examples
     public class Examples
     {
         [Fact]
-        public void Test1()
+        public void NullableReferenceTypeExamples()
         {
-
+            string notNullable = "hello";
+            string? nullable = default;
+            
+            Assert.Equal("hello", notNullable);
+            Assert.Null(nullable);
+            
+            Assert.True(notNullable.Length > 0);
+            
+            var nullReference = Record.Exception(
+                // () => nullable.Length > 0); // warning CS8602: Dereference of a possibly null reference.
+                () => nullable!.Length > 0);
+            Assert.IsType<NullReferenceException>(nullReference);
+            
+            var isNullExample = Record.Exception(() =>
+                {
+                    if (nullable is null)
+                      return true;
+                    else
+                      return nullable.Length > 0;
+                });
+            Assert.Null(isNullExample);
+            
+            var needsHelpExample = Record.Exception(() =>
+                {
+                    if (string.IsNullOrEmpty(nullable))
+                      return true;
+                    else
+                    //   return nullable.Length > 0; // warning CS8602: Dereference of a possibly null reference.
+                      return nullable is null ? false : nullable.Length > 0;
+                });
+            Assert.Null(needsHelpExample);
+            
+            var methodExample = Record.Exception(() =>
+                {
+                    if (!IsValid(nullable))
+                      return true;
+                    else
+                      return nullable.Length > 0;
+                    
+                    bool IsValid([NotNullWhen(true)] string? s)
+                      => !string.IsNullOrEmpty(s);
+                });
+            Assert.Null(methodExample);
         }
     }
 }

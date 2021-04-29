697a8e8b3f486b06d422781fa6d5681f88716360
Mike Harris <MikeMKH@users.noreply.github.com>
2021-04-29 06:21:41 -0500

    add ch 15.2

--- ch15/Examples/Examples.cs
+++ ch15/Examples/Examples.cs
@@ -53,5 +53,43 @@ namespace Examples
                 });
             Assert.Null(methodExample);
         }
+        
+        [Theory]
+        [InlineData(0, "FizzBuzz")]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(33, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(55, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(45, "FizzBuzz")]
+        [InlineData(7, "7")]
+        [InlineData(77, "77")]
+        public void FizzBuzzSwitchExpression(int value, string expected)
+        {
+            var actual = FizzBuzz(value);
+            Assert.Equal(expected, actual);
+            
+            var whenActual = FizzBuzz_WithWhen(value);
+            Assert.Equal(expected, whenActual);
+            
+            string FizzBuzz(int n)
+              => (n % 3, n % 5) switch
+              {
+                  (0, 0) => "FizzBuzz",
+                  (0, _) => "Fizz",
+                  (_, 0) => "Buzz",
+                  _ => n.ToString()
+              };
+              
+            string FizzBuzz_WithWhen(int n)
+              => n switch
+              {
+                  int m when (m % 3 == 0) && (m % 5 == 0) => "FizzBuzz",
+                  int m when (m % 3 == 0) && (m % 5 != 0) => "Fizz",
+                  int m when (m % 3 != 0) && (m % 5 == 0) => "Buzz",
+                  _ => n.ToString()  
+              };
+        }
     }
 }
